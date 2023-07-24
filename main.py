5307e89f9855f5f8488cf5eb544a921a1406c046
rodcordova <rodrigocordoba638@gmail.com>
2023-07-24 02:06:15 -0300

    se completa la funcion character

--- main.py
+++ main.py
@@ -175,5 +175,21 @@ def recomendacion_endpoint(nombre_pelicula: str):
 def character(id:str):
     respuesta={
         'id': 1, 
-        'name':'Rick Sanchez'}
+        'name':'Rick Sanchez',
+        'status': 'Alive', 
+        'species': 'Human',
+        'type': "",
+        'gender': "Male",
+        'origin': {
+            'name': "Earth (C-137)",
+            'url':"https://rickandmortyapi.com/api/location/1"
+            },
+        'location': {
+            'name': "Citadel of Ricks",
+            'url': "https://rickandmortyapi.com/api/location/3"
+            },
+        'image':"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        'url': "https://rickandmortyapi.com/api/character/1",
+        'created':"2017-11-04T18:48:46.250Z"
+        }
     return respuesta
